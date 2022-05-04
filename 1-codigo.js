8db7647e4ac1f706794a545d86efcf251ea3da2b
ssarinha <102757853+ssarinha@users.noreply.github.com>
2022-05-04 16:48:01 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,14 @@
+
+//console.log(12+20);
+//console.log(25+157);
+//console.log(30+16);
+//console.log('Sara' + ' ' + 'de' + 'Sousa' + 'Lima');
+
+const nome = ('Sara');
+console.log (nome);
+
+const a=1500;
+const b=1200;
+const c=5;
+
+console.log (a+b)
---
+++ exercícios.js
@@ -0,0 +1,30 @@
+// 1-a
+console.log (10 + 20 * 30)
+
+// 1-b
+console.log (4**2/30)
+
+//1-c
+console.log ((9**4+2)*6-1)
+
+//2
+console.log (10%3*10**1+1-10*4/2)
+
+//3
+console.log('Sara' + ' ' + 'de' + ' ' + 'Sousa' + ' ' + 'Lima');        
+
+//4
+const x=3
+const y=5
+
+console.log (2*x*3*y)
+
+//5
+const a=1;
+const b=2;
+const c=3
+console.log (a+b+c)
+
+//6
+const d=1800;
+console.log (d*2)
